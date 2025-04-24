ebc00f2833427c8a23978a6868b5931abec2bbbf
remeq7 <remremo20a@gmail.com>
2025-04-24 15:48:09 +0200

    Setup Cypress with initial test and .gitignore

---
+++ cypress/e2e/page_test.cy.js
@@ -0,0 +1,31 @@
+describe("Testowanie strony głównej https://remeq7.github.io/Page-to-test/index.html", () => {
+  beforeEach(() => {
+    cy.visit("https://remeq7.github.io/Page-to-test/index.html");
+  });
+
+  it("powinien poprawnie wypełnić i wysłać formularz", () => {
+    // Wypełnij pole Imię
+    cy.get('input[name="imie"]').type("Jan Kowalski");
+
+    // Wypełnij pole Email
+    cy.get('input[name="email"]').type("jan.kowalski@example.com");
+
+    // Wypełnij pole Hasło
+    cy.get('input[name="haslo"]').type("tajnehaslo");
+
+    // Wybierz płeć Mężczyzna
+    cy.get('input[name="plec"][value="mezczyzna"]').check();
+
+    // Wybierz język Polski z dropdownu
+    cy.get('select[name="jezyk"]').select("Polski");
+
+    // Zaznacz zainteresowanie Programowanie
+    cy.get('input[name="zainteresowania"][value="programowanie"]').check();
+
+    // Wpisz wiadomość
+    cy.get('textarea[name="wiadomosc"]').type("To jest testowa wiadomość.");
+
+    // Kliknij przycisk Wyślij
+    cy.get('button[type="submit"]').click();
+  });
+});
